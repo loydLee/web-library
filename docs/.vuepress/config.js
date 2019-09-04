4ff6baa1f372f16b914c7311c8d5db27d30d2e1b
lloydLee <lil@huidiancloud.com>
2019-09-04 15:28:21 +0800

    icon  add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'loydLee\'s home',
   description: '我的小窝',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/favicon.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   base: '/', // 这是部署到github相关的配置
   markdown: {
